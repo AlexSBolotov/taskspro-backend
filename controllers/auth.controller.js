904d661a0f80c52d2fa101107524fab4d4f07f6d
Yaroslav Klimas <yarklim@gmail.com>
2023-07-30 13:31:20 +0300

    created authorisation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,8 @@
-const { HttpError, ctrlWrapper } = require("../helpers");
-const { User } = require("../models/user.model");
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const { HttpError, ctrlWrapper } = require('../helpers');
+const { User } = require('../models/user.model');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const { SECRET_KEY } = process.env;
 
@@ -11,14 +11,31 @@ const registerUser = async (req, res) => {
 
   const user = await User.findOne({ email });
   if (user) {
-    throw HttpError(409, "Email in use");
+    throw HttpError(409, 'Email in use');
   }
   const hashPassword = await bcryptjs.hash(password, 10);
-  const result = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL: '',
+    activeBoard: '',
+  });
+
+  const { _id: id } = newUser;
+
+  const payload = { id };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' });
+
+  await User.findByIdAndUpdate(id, { token });
+
   res.status(201).json({
     user: {
-      email: result.email,
-      subscription: result.subscription,
+      name: newUser.name,
+      email: newUser.email,
+      theme: newUser.theme,
+      avatarURL: newUser.avatarURL,
+      activeBoard: newUser.activeBoard,
     },
   });
 };
@@ -27,52 +44,59 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    throw HttpError(401, "Email or password is wrong");
+    throw HttpError(401, 'Email or password is wrong');
   }
   const comparePassword = await bcryptjs.compare(password, user.password);
   if (!comparePassword) {
-    throw HttpError(401, "Email or password is wrong");
+    throw HttpError(401, 'Email or password is wrong');
   }
   const payload = { id: user._id };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '24h' });
   await User.findByIdAndUpdate(user._id, { token });
   res.status(200).json({
     token: token,
     user: {
+      name: user.name,
       email: user.email,
-      subscription: user.subscription,
+      theme: user.theme,
+      avatarURL: user.avatarURL,
+      activeBoard: user.activeBoard,
     },
   });
 };
 
 const getCurrentUser = async (req, res) => {
-  const { email, subscription } = req.user;
+  const { email, name, theme, avatarURL, activeBoard } = req.user;
   res.status(200).json({
+    name,
     email,
-    subscription,
+    theme,
+    avatarURL,
+    activeBoard,
   });
 };
 
 const logoutUser = async (req, res) => {
   const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: "" });
+  await User.findByIdAndUpdate(_id, { token: '' });
   res.status(204).json({
-    message: "Logout success",
+    message: 'Logout success',
   });
 };
-const updateUserSubscription = async (req, res) => {
-  const { _id } = req.user;
-  const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
-  res.status(200).json({
-    message: "Subscription successfully changed",
-    currentSubscription: result.subscription,
-  });
-};
+
+// const updateUserSubscription = async (req, res) => {
+//   const { _id } = req.user;
+//   const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
+//   res.status(200).json({
+//     message: 'Subscription successfully changed',
+//     currentSubscription: result.subscription,
+//   });
+// };
 
 module.exports = {
   registerUser: ctrlWrapper(registerUser),
   loginUser: ctrlWrapper(loginUser),
   getCurrentUser: ctrlWrapper(getCurrentUser),
   logoutUser: ctrlWrapper(logoutUser),
-  updateUserSubscription: ctrlWrapper(updateUserSubscription),
+  //   updateUserSubscription: ctrlWrapper(updateUserSubscription),
 };
--- models/user.model.js
+++ models/user.model.js
@@ -1,67 +1,90 @@
-const { Schema, model } = require("mongoose");
-const Joi = require("joi");
-const { handleMongooseError } = require("../helpers");
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+const { handleMongooseError } = require('../helpers');
 
-const emailRegexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-const themeTypes = ["light", "dark", "violet"];
+const emailRegexp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const passRegex = /^[a-zA-Z0-9]+$/;
+const themeTypes = ['light', 'dark', 'violet'];
 
 const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, "Username is required"],
+      required: [true, 'Username is required'],
     },
     password: {
       type: String,
-      required: [true, "Set password for user"],
+      required: [true, 'Set password for user'],
     },
     email: {
       type: String,
       match: emailRegexp,
-      required: [true, "Email is required"],
+      required: [true, 'Email is required'],
       unique: true,
     },
     theme: {
       type: String,
       enum: themeTypes,
-      default: "light",
+      default: 'dark',
     },
     avatarURL: {
       type: String,
     },
+    activeBoard: {
+      type: String,
+      default: '',
+    },
     token: {
       type: String,
-      default: "",
+      default: '',
     },
   },
-  { versionKey: false }
+  { versionKey: false, timestamps: true }
 );
 
-userSchema.post("save", handleMongooseError);
+userSchema.post('save', handleMongooseError);
 
-const authUserSchema = Joi.object({
+const registerUserSchema = Joi.object({
   name: Joi.string().required().messages({
-    "any.required": "missing required name field",
+    'string.pattern.base': 'Name can only contain Latin letters, numbers',
+    'any.required': 'Name is required',
   }),
   email: Joi.string().pattern(emailRegexp).required().messages({
-    "any.required": "missing required email field",
+    'string.pattern.base': 'Email must be a valid email address',
+    'any.required': 'Email is required',
   }),
-  password: Joi.string().required().messages({
-    "any.required": "missing required password field",
+  password: Joi.string().min(8).max(32).pattern(passRegex).required().messages({
+    'string.pattern.base': 'Password can only contain Latin letters, numbers',
+    'string.min': 'Password must be at least {#limit} characters long',
+    'string.max': 'Password must not exceed {#limit} characters',
+    'any.required': 'Password is required',
   }),
   theme: Joi.string().valid(...themeTypes),
 });
-const updateUserSubscription = Joi.object({
-  subscription: Joi.string()
-    .valid(...themeTypes)
-    .required(),
+
+const loginUserSchema = Joi.object({
+  password: Joi.string().required().messages({
+    'any.required': 'You must enter a password.',
+  }),
+  email: Joi.string().required().messages({
+    'any.required': 'You must enter a email',
+  }),
 });
 
+// const updateUserSubscription = Joi.object({
+//   subscription: Joi.string()
+//     .valid(...themeTypes)
+//     .required(),
+// });
+
 const schemas = {
-  authUserSchema,
-  updateUserSubscription,
+  registerUserSchema,
+  loginUserSchema,
+  //   updateUserSubscription,
 };
-const User = model("user", userSchema);
+
+const User = model('user', userSchema);
 
 module.exports = {
   User,
